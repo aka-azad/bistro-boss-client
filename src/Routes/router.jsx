3f059d67101e38507f9c88f581d4d8695500377b
Ashraf Azad <ashrafazad86@gmail.com>
2024-12-29 17:20:15 +0600

    first commit

---
+++ src/Routes/router.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: '/register',
+            element: <></>
+        },
+        {
+            path: '/login',
+            element: <></>
+        },
+        {
+            path: '/our-shop',
+            element: <></>
+        },
+        {
+            path: '/our-menu',
+            element: <></>
+        },
+        {
+            path: '/contact-us',
+            element: <></>
+        },
+    ],
+  },
+]);
